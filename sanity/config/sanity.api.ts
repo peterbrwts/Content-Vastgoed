dd0d273571e5c61399cfdc9ccfaf7399bfa35334
Peter Berwouts <peterberwouts@icloud.com>
2024-07-15 18:43:14 +0200

    new api

--- sanity/config/sanity.api.ts
+++ sanity/config/sanity.api.ts
@@ -2,12 +2,12 @@ export const useCdn = false
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET',
+  'production',
 )
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID',
+  'nyjc2nrg',
 )
 
 export const readToken = process.env.SANITY_API_READ_TOKEN || ''
