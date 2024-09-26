c5d34b114b9675ecfef09cdcb689c035c50158e3
Berwouts <50912949+peterbrwts@users.noreply.github.com>
2024-09-26 14:45:58 +0200

    update san

--- sanity/config/sanity.api.ts
+++ sanity/config/sanity.api.ts
@@ -2,12 +2,12 @@ export const useCdn = false
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'production',
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET',
 )
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'nyjc2nrg',
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID',
 )
 
 export const readToken = process.env.SANITY_API_READ_TOKEN || ''
