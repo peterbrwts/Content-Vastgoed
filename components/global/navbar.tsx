319717b391292b4a29a1e0a75500121e6b685cfd
Peter Berwouts <peterberwouts@icloud.com>
2024-10-08 20:16:38 +0200

    add logo

--- components/global/navbar.tsx
+++ components/global/navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { SettingsPayload } from '@/types'
-import Logo from '@/components/shared/logo'
+import Logo from '../../public/icons/logo.svg'
 import Button from '@/components/shared/button'
 
 interface NavbarProps {
@@ -16,7 +16,10 @@ export default function Navbar(props: NavbarProps) {
       className='z-50 hidden sticky top-0 md:flex backdrop-blur-sm items-center justify-between max-w-8xl mx-auto my-0 py-6 px-6 md:px-12 rounded-bl-xl rounded-br-xl border-b border-b-[#efeff1] bg-[#ffffff] bg-opacity-80'
     >
       <div className='text-primary'>
-      <Logo text={settings.logoText} />
+      <Link href="/" className='text-2xl uppercase tracking-widest transition hover:scale-[0.96]'>
+      <Logo width={50} height={50} />
+    </Link>
+     
       </div>
       <nav>
         <ul className='hidden md:flex gap-12 items-center list-none'>
--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -1,16 +1,25 @@
 import Link from 'next/link'
+import Image from 'next/image'
+
 
 interface LogoProps {
-  text: string
+  image: string
 }
 
 export default function Logo(props: LogoProps) {
 
-  const { text } = props
+  const { image } = props
 
   return (
     <Link href="/" className='text-2xl uppercase tracking-widest transition hover:scale-[0.96]'>
-      {text}
+      <Image 
+              src={image}
+              width={800}
+              height={800}
+              sizes="(max-width: 1000px) 2000px"
+              alt='Guy Content'
+              className='mb-10 md:mb-0 mt-10 md:mt-20 object-contain'
+            />
     </Link>
   )
 }
